7df1cce7af5d751e157a877aee0c858851690904
yael-mar <elmardi.yas@gmail.com>
2026-08-10 23:13:28 +0200

    Final Commit C02

--- Yassine/C02/ex09/ft_strcapitalize.c
+++ Yassine/C02/ex09/ft_strcapitalize.c
@@ -12,5 +12,22 @@
 
 char	ft_strcapitalize(char *str)
 {
-	
+	int	i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		if(str[i] >= 97 && str[i] <= 122)
+		{
+			if(i == 0 || (str[i - 1] < 48 || (str[i - 1] > 57 && str[i - 1] < 65) || (str[i - 1] > 90 && str[i - 1] < 97) || str[i - 1] > 122))
+				str[i] -= 32;
+		}
+		else if(str[i] >= 65 && str[i] <= 90)
+		{
+			if(i != 0 && ((str[i - 1] >= 48 && str[i - 1] <= 57) || (str[i - 1] >= 65 && str[i - 1] <= 90) || (str[i - 1] >= 97 && str[i - 1] <= 122)))
+				str[i] += 32;
+		}
+		i++;
+	}
+	return(str);
 }
---
+++ Yassine/C02/ex10/ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yasel-ma <elmardiyassine00@gmail.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/10 23:12:00 by yasel-ma          #+#    #+#             */
+/*   Updated: 2026/08/10 23:12:00 by yasel-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int res;
+
+	i = 0;
+	res = 0;
+	while (src[res])
+		res++;
+	if (size < 1)
+		return (res);
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (res);
+}
